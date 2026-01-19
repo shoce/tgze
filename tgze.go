ba0c31b07506790a7594b5e81fc16d50b574d8c7
shoce <yogze@icloud.com>
2026-01-19 23:53:26 +0700

    fmt

--- tgze.go
+++ tgze.go
@@ -701,7 +701,7 @@ func processTgUpdate(u tg.Update, tgupdatesjson string) (m tg.Message, err error
 			ChatId:           fmt.Sprintf("%d", m.Chat.Id),
 			ReplyToMessageId: m.MessageId,
 			Text: tg.Code(tg.F(
-				"@ReplyToMessage { @MessageId <%d> @Audio {%v} }",
+				"@ReplyToMessage { @MessageId <%d> @Audio %+v }",
 				m.ReplyToMessage.MessageId, m.ReplyToMessage.Audio,
 			)),
 		}); tgerr != nil {
@@ -726,7 +726,7 @@ func processTgUpdate(u tg.Update, tgupdatesjson string) (m tg.Message, err error
 			ChatId:           fmt.Sprintf("%d", m.Chat.Id),
 			ReplyToMessageId: m.MessageId,
 			Text: tg.Code(tg.F(
-				"File { @FileSize <%d> @FilePath [%s] }",
+				"@File { @FileSize <%d> @FilePath [%s] }",
 				file.FileSize, file.FilePath,
 			)),
 		}); tgerr != nil {
